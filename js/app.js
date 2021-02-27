3b56442d37e080d4f00855e8900ada5e2490af69
Stygan <javier.stuca@gmail.com>
2021-02-27 15:49:59 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const random = getRandomInt(1, 151);
+  fetchData(random);
+});
+
+const fetchData = async (id) => {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+
+    const pokemon = {
+      img: data.sprites.other.dream_world.front_default,
+      name: data.name,
+      hp: data.stats[0].base_stat,
+      exp: data.base_experience,
+      atk: data.stats[1].base_stat,
+      def: data.stats[2].base_stat,
+      spc: data.stats[3].base_stat,
+    };
+
+    renderCard(pokemon);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderCard = (pokemon) => {
+  const flex = document.querySelector(".flex");
+  const template = document.querySelector("#template-card").content;
+  const clone = template.cloneNode(true);
+  const fragment = document.createDocumentFragment();
+
+  clone.querySelector(".card-body-img").setAttribute("src", pokemon.img);
+  clone.querySelector(
+    ".card-body-title"
+  ).innerHTML = `${pokemon.name} <span>${pokemon.hp} hp</span>`;
+  clone.querySelector(".card-body-text").textContent = "Exp. " + pokemon.exp;
+  clone.querySelectorAll(".card-footer-social h3")[0].textContent = pokemon.atk;
+  clone.querySelectorAll(".card-footer-social h3")[1].textContent = pokemon.spc;
+  clone.querySelectorAll(".card-footer-social h3")[2].textContent = pokemon.def;
+
+  fragment.appendChild(clone);
+  flex.appendChild(fragment);
+};
